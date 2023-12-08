f92b0167ce41887af031da7f9073dc8b4d467f16
unknown <zhych0111@gmail.com>
2023-12-08 21:33:27 +0800

    add lambda

---
+++ Java-SE/src/main/java/com/cqupt/lambda/LambdaTest.java
@@ -0,0 +1,22 @@
+package com.cqupt.lambda;
+
+import java.util.Arrays;
+
+public class LambdaTest {
+    public static void main(String[] args) {
+        Swimming s = () -> {
+            System.out.println("Hello");
+        };
+        s.swim();
+
+        Integer[] a = new Integer[]{1, 2, 6, 5, 9, 6};
+        Arrays.sort(a, (o1, o2)->(Integer.compare(o1, o2)));
+        System.out.println(Arrays.toString(a));
+    }
+}
+
+// 只能简化函数式接口的匿名内部类！！！
+@FunctionalInterface
+interface Swimming {
+    void swim();
+}
---
+++ Java-SE/src/main/java/com/cqupt/lambda/RankWords.java
@@ -0,0 +1,12 @@
+package com.cqupt.lambda;
+
+import java.util.Arrays;
+
+public class RankWords {
+    public static void main(String[] args) {
+        // 方法引用 
+        String[] names = {"body", "apple", "Andy", "Cici", "Delete", "zebra", "Oppo", "huawei"};
+        Arrays.sort(names, String::compareToIgnoreCase);
+        System.out.println(Arrays.toString(names));
+    }
+}
